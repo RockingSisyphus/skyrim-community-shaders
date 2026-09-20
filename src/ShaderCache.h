e902a501caab1378a603328be7d2c0c55b9cc121
RockingSisyphus <rockingsisyphus@qq.com>
2026-09-20 12:33:25 +0800

    fix(shaders): synchronize background mode reads

--- src/ShaderCache.h
+++ src/ShaderCache.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <BS_thread_pool.hpp>
+#include <atomic>
 #include <efsw/efsw.hpp>
 #include <vector>
 
@@ -537,7 +538,7 @@ namespace SIE
 		int32_t backgroundCompilationThreadCount = std::max(static_cast<int32_t>(Util::GetPerformanceCoreCount()) / 2, 1);
 		BS::thread_pool<> compilationPool{ static_cast<std::size_t>(compilationThreadCount) };
 		std::jthread managementJthread;  // dedicated thread for ManageCompilationSet (not in pool)
-		bool backgroundCompilation = false;
+		std::atomic_bool backgroundCompilation{ false };
 		bool menuLoaded = false;
 
 		enum class LightingShaderTechniques
